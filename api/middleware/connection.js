cebe2e042fa2f8916d23c44d08fbea52b8833ccf
unknown <sandeep.n062000@gmail.com>
2024-11-01 18:44:31 +0530

    feat: Update connection.js

--- api/middleware/connection.js
+++ api/middleware/connection.js
@@ -8,12 +8,4 @@ const db = mysql.createPool({
   database: process.env.DB_NAME,
 });
 
-db.connect((err) => {
-  if (err) {
-    console.error('Database connection failed:', err);
-    return;
-  }
-  console.log('Connected to MySQL');
-});
-
 module.exports = db;
