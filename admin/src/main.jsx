ca5e8ae86d91265477c5d6caa37694f9aa6a74b9
unknown <sandeep.n062000@gmail.com>
2024-11-11 22:15:11 +0530

    feat: app.js

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -1,12 +1,14 @@
-import 'bootstrap/dist/css/bootstrap.min.css';  // Bootstrap CSS
-import './index.css';  // Tailwind CSS
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css';  // Bootstrap CSS
+import { BrowserRouter } from 'react-router-dom'
+import './index.css';  // Tailwind CSS
 
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
 )
