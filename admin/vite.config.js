9a7c512c69f312669c33fbf81cc721ab2a6b00e5
unknown <sandeep.n062000@gmail.com>
2024-11-12 10:33:46 +0530

    Update vite.config.js

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -4,5 +4,4 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Adjust this if your app is in a subdirectory
 })
